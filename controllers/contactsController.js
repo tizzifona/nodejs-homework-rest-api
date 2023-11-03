540daedff5f1a541901900dd23da0436d7b988b1
Nadia Alaieva <tizzifona@gmail.com>
2023-11-03 23:05:28 +0100

    Update contactsController.js
    Added filter: favorite=true

--- controllers/contactsController.js
+++ controllers/contactsController.js
@@ -3,12 +3,20 @@ const {HttpError} = require('../helpers');
 const {ctrlWrapper} = require('../decorators/ctrlWraper');
 
 const getAll = async (req, res) => {
-    const {_id:owner} = req.user;
-    const {page = 1, limit = 20} = req.query;
-    const skip = (page -1)*limit;
-    const result = await Contact.find({owner}, '-createdAt -updatedAt', {skip, limit}).populate('owner', 'username email');
+    const {_id: owner} = req.user;
+    const { page = 1, limit = 20, favorite } = req.query;
+    const skip = (page - 1) * limit;
+
+    const filter = { owner };
+
+    if (favorite) {
+        filter.favorite = favorite === 'true';
+    }
+
+    const result = await Contact.find(filter, '-createdAt -updatedAt', { skip, limit }).populate('owner', 'username email');
     res.json(result);
 }
+
 
 const getById = async (req, res) => {
         const {_id:owner} = req.user;
